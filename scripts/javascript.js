a90ad8a1b8df07da2031fac38905f990c8b02c14
Robert Brandt <robert.brandt@hotmail.fi>
2021-10-14 11:37:07 +0300

    Javascript versio valmis

---
+++ scripts/javascript.js
@@ -0,0 +1,9 @@
+
+function loginClicked() {
+    let firstName, surName;
+
+    firstName = document.getElementById("firstName").value;
+    surName = document.getElementById("surName").value;
+
+    alert(`Töttöröö, olet ${firstName} ${surName}`);
+}
